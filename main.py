eaf2ae1cb47eb8901557639eb5596844ebdd97cd
rodolphe <rodolphe.nakhle@gmail.com>
2024-04-30 23:52:01 +0200

    adding detail_print

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from sub import my_print
+from sub import *
 
 my_print("Tolito")
 l=[1,2,3]
 for i in l:
     my_print(str(i) + " ")
+    detail_print(i)
 my_print("done")
--- sub.py
+++ sub.py
@@ -1,2 +1,8 @@
 def my_print(s):
     print(s)
+
+def detail_print(s):
+    print(s , end='')
+    print(" is a ", end='')
+    print(type(s))
+
